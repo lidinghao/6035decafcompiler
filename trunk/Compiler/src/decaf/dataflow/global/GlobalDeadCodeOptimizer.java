8ff03ff99c43807e834e8d6d2eaad5b6741a436c
usmasood@gmail.com <usmasood@gmail.com@3a3f519a-766e-0e4b-19c7-2359dfd7c0c6>
2011-05-01 21:16:16 +0000

    Fixing bug with not reset for exception handler

--- trunk/Compiler/src/decaf/dataflow/global/GlobalDeadCodeOptimizer.java
+++ trunk/Compiler/src/decaf/dataflow/global/GlobalDeadCodeOptimizer.java
@@ -66,13 +66,13 @@ public class GlobalDeadCodeOptimizer {
 			stmt = block.getStatements().get(i);
 			if (stmt.getClass().equals(CallStmt.class)) {
 				CallStmt callStmt = (CallStmt) stmt;
-				if (callStmt.getMethodLabel().equals(ProgramFlattener.exceptionHandlerLabel)) continue;
-				
-				// Set all global variable IDs to true in the out set, so
-				// above statements have a more accurate view of the out set
-				// because it includes things that happen within the block
-				for (int globalVarId : globalVarIDs) {
-					bFlow.getOut().set(globalVarId);
+				if (!callStmt.getMethodLabel().equals(ProgramFlattener.exceptionHandlerLabel)) {
+					// Set all global variable IDs to true in the out set, so
+					// above statements have a more accurate view of the out set
+					// because it includes things that happen within the block
+					for (int globalVarId : globalVarIDs) {
+						bFlow.getOut().set(globalVarId);
+					}
 				}
 			}
 			if (stmt.getClass().equals(QuadrupletStmt.class)) {
