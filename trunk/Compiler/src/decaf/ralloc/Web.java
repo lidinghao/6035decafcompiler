22f45a595300fd1a49ae5d1e756f591034309b6c
usmasood@gmail.com <usmasood@gmail.com@3a3f519a-766e-0e4b-19c7-2359dfd7c0c6>
2011-05-02 17:57:58 +0000

    Web fix

--- trunk/Compiler/src/decaf/ralloc/Web.java
+++ trunk/Compiler/src/decaf/ralloc/Web.java
@@ -3,9 +3,15 @@ package decaf.ralloc;
 import java.util.ArrayList;
 import java.util.List;
 
+import decaf.codegen.flatir.CmpStmt;
 import decaf.codegen.flatir.LIRStatement;
+import decaf.codegen.flatir.LoadStmt;
 import decaf.codegen.flatir.Name;
+import decaf.codegen.flatir.PopStmt;
+import decaf.codegen.flatir.PushStmt;
+import decaf.codegen.flatir.QuadrupletStmt;
 import decaf.codegen.flatir.Register;
+import decaf.codegen.flatir.StoreStmt;
 
 public class Web {
 	private Name variable;
@@ -42,7 +48,21 @@ public class Web {
 		for (LIRStatement stmt: this.definitions) {
 			if (stmt == definition) return;
 		}
+		
+		processDefinition(definition);
+		
 		this.definitions.add(definition);
+	}
+
+	private void processDefinition(LIRStatement definition) {
+		if (definition.getClass().equals(LoadStmt.class)) {
+			LoadStmt lStmt = (LoadStmt) definition;
+			lStmt.setVariable(this.variable);
+		}
+		else if (definition.getClass().equals(QuadrupletStmt.class)) {
+			QuadrupletStmt qStmt = (QuadrupletStmt) definition;
+			qStmt.setDestination(this.variable);
+		}
 	}
 
 	public List<LIRStatement> getUses() {
@@ -57,7 +77,49 @@ public class Web {
 		for (LIRStatement stmt: this.uses) {
 			if (stmt == use) return;
 		}
+		
+		processUse(use);
+		
 		this.uses.add(use);
+	}
+
+	private void processUse(LIRStatement use) {
+		if (use.getClass().equals(QuadrupletStmt.class)) {
+			QuadrupletStmt qStmt = (QuadrupletStmt) use;
+			if (this.variable.equals(qStmt.getArg1())) {
+				qStmt.setArg1(this.variable);
+			}
+			else if (this.variable.equals(qStmt.getArg2())) {
+				qStmt.setArg2(this.variable);
+			}
+		}
+		else if (use.getClass().equals(CmpStmt.class)) {
+			CmpStmt cStmt = (CmpStmt) use;
+			if (this.variable.equals(cStmt.getArg1())) {
+				cStmt.setArg1(this.variable);
+			}
+			else if (this.variable.equals(cStmt.getArg2())) {
+				cStmt.setArg2(this.variable);
+			}
+		}
+		else if (use.getClass().equals(PushStmt.class)) {
+			PushStmt pStmt = (PushStmt) use;
+			if (pStmt.getName().equals(this.variable)) {
+				pStmt.setName(this.variable);
+			}
+		}
+		else if (use.getClass().equals(PopStmt.class)) {
+			PopStmt pStmt = (PopStmt) use;
+			if (pStmt.getName().equals(this.variable)) {
+				pStmt.setName(this.variable);
+			}
+		}
+		else if (use.getClass().equals(StoreStmt.class)) {
+			StoreStmt sStmt = (StoreStmt) use;
+			if (sStmt.getVariable().equals(this.variable)) {
+				sStmt.setVariable(this.variable);
+			}
+		}
 	}
 
 	public Register getRegister() {
@@ -93,17 +155,17 @@ public class Web {
 		return firstStmtIndex;
 	}
 	
-	public void combineWeb(Web w) {
-		List<LIRStatement> usesToAdd = new ArrayList<LIRStatement>();
-		List<LIRStatement> defsToAdd = new ArrayList<LIRStatement>();
-		
+	public void combineWeb(Web w) {		
 		for (LIRStatement s1: w.getUses()) {
 			boolean add = true;
 			for (LIRStatement s2: this.uses) {
 				if (s1 == s2) add = false;
 			}
 			
-			if (add) usesToAdd.add(s1);
+			if (add) {
+				processUse(s1);
+				this.uses.add(s1);
+			}
 		}
 		
 		for (LIRStatement s1: w.getDefinitions()) {
@@ -112,10 +174,10 @@ public class Web {
 				if (s1 == s2) add = false;
 			}
 			
-			if (add) defsToAdd.add(s1);
+			if (add) {
+				processDefinition(s1);
+				this.definitions.add(s1);
+			}
 		}
-		
-		this.uses.addAll(usesToAdd);
-		this.definitions.addAll(defsToAdd);
 	}
 }
