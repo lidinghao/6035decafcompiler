0044b7a86402cfd41be5f8447e5b06f9bd55d180
usmasood <usmasood@gmail.com>
2011-05-07 01:40:41 +0000

    Load/Store stuff

--- Compiler/src/decaf/memory/LiveGlobalStores.java
+++ Compiler/src/decaf/memory/LiveGlobalStores.java
@@ -19,22 +19,23 @@ import decaf.dataflow.cfg.CFGBlock;
 import decaf.dataflow.cfg.MethodIR;
 import decaf.dataflow.global.BlockDataFlowState;
 
-public class ReachingStoresAnalyzer {
+public class LiveGlobalStores {
 	private HashSet<CFGBlock> cfgBlocksToProcess;
 	private HashMap<CFGBlock, BlockDataFlowState> cfgBlocksState;
 	private HashMap<String, MethodIR> mMap;
-	private HashMap<String, List<Name>> uniqueStores;
+	private HashMap<String, List<Name>> uniqueGlobals;
 	
-	public ReachingStoresAnalyzer(HashMap<String, MethodIR> mMap) {
+	public LiveGlobalStores(HashMap<String, MethodIR> mMap) {
 		this.mMap = mMap;
 		this.cfgBlocksToProcess = new HashSet<CFGBlock>();
 		this.cfgBlocksState = new HashMap<CFGBlock, BlockDataFlowState>();
-		this.uniqueStores = new HashMap<String, List<Name>>();
+		this.uniqueGlobals = new HashMap<String, List<Name>>();
 	}
 	
+	// Each QuadrupletStmt will have a unique ID
 	public void analyze() {
 		this.cfgBlocksState.clear();
-		this.uniqueStores.clear();
+		this.uniqueGlobals.clear();
 		
 		for (String methodName: this.mMap.keySet()) {
 			if (methodName.equals(ProgramFlattener.exceptionHandlerLabel)) continue;
@@ -45,15 +46,15 @@ public class ReachingStoresAnalyzer {
 	}
 
 	private void runWorkList(String methodName) {
-		int totalGlobals = this.uniqueStores.get(methodName).size();
+		int totalGlobals = this.uniqueGlobals.get(methodName).size();
 		
-		CFGBlock entry = this.getBlockById(methodName, 0);
-		BlockDataFlowState entryBlockFlow = new BlockDataFlowState(totalGlobals); // OUT = GEN for entry block
-		calculateGenKillSets(entry, entryBlockFlow);
-		entryBlockFlow.setOut(entryBlockFlow.getGen());
-		cfgBlocksToProcess.remove(entry);
+		CFGBlock exit = this.getExitBlock(methodName);
+		BlockDataFlowState entryBlockFlow = new BlockDataFlowState(totalGlobals); // IN = GEN for entry block
+		calculateGenKillSets(exit, entryBlockFlow);
+		entryBlockFlow.setIn(entryBlockFlow.getGen());
+		cfgBlocksToProcess.remove(exit);
 		
-		this.cfgBlocksState.put(entry, entryBlockFlow);
+		this.cfgBlocksState.put(exit, entryBlockFlow);
 		
 		while (cfgBlocksToProcess.size() != 0) {
 			CFGBlock block = (CFGBlock)(cfgBlocksToProcess.toArray())[0];
@@ -62,11 +63,9 @@ public class ReachingStoresAnalyzer {
 		}		
 	}
 	
-	private CFGBlock getBlockById(String name, int i) {
-		if (this.mMap.containsKey(name)) {
-			for (CFGBlock b: this.mMap.get(name).getCfgBlocks()) {
-				if (b.getIndex() == i) return b;
-			}
+	private CFGBlock getExitBlock(String methodName) {
+		for (CFGBlock block: this.mMap.get(methodName).getCfgBlocks()) {
+			if (block.getSuccessors().size() == 0) return block;
 		}
 		
 		return null;
@@ -83,57 +82,62 @@ public class ReachingStoresAnalyzer {
 				if (stmt.getClass().equals(QuadrupletStmt.class)) {
 					QuadrupletStmt qStmt = (QuadrupletStmt)stmt;
 					
-					// Only care about global assignments
 					if (qStmt.getDestination().isGlobal()) temp.add(qStmt.getDestination());
 				}
+				else if (stmt.getClass().equals(StoreStmt.class)) {
+					StoreStmt sStmt = (StoreStmt)stmt;
+					temp.add(sStmt.getVariable());
+				}
+				
 			}
 			
 			this.cfgBlocksToProcess.add(block);
 		}
 		
-		this.uniqueStores.put(methodName, new ArrayList<Name>());
-		this.uniqueStores.get(methodName).addAll(temp);
+		this.uniqueGlobals.put(methodName, new ArrayList<Name>());
+		this.uniqueGlobals.get(methodName).addAll(temp);
 	}
 	
 	private BlockDataFlowState generateDFState(CFGBlock block) {
-		int totalGlobals = this.uniqueStores.get(block.getMethodName()).size();
-		// Get the original IN BitSet for this block
+		int totalGlobals = this.uniqueGlobals.get(block.getMethodName()).size();
+		
+		// Get the original inBitSet for this block
 		BitSet origIn;
 		if (this.cfgBlocksState.containsKey(block)) {
-			origIn = this.cfgBlocksState.get(block).getOut();
+			origIn = this.cfgBlocksState.get(block).getIn();
 		} else {
 			origIn = new BitSet(totalGlobals);
 			origIn.set(0, totalGlobals);
 		}
 		
-		// Calculate the in BitSet by taking intersection of successor INs
+		// Calculate the in BitSet by taking intersection of predecessors
 		BlockDataFlowState bFlow = new BlockDataFlowState(totalGlobals);
 		
-		// If there exists at least one successor, set OUT to all True
+		// If there exists no successor, set Out to all True
 		if (block.getSuccessors().size() > 0) {
 			bFlow.getOut().set(0, totalGlobals);
 		}
 		
 		BitSet out = bFlow.getOut();
-		for (CFGBlock succ : block.getSuccessors()) {
-			if (this.cfgBlocksState.containsKey(succ)) {
-				out.and(this.cfgBlocksState.get(succ).getIn());
+		for (CFGBlock pred : block.getPredecessors()) {
+			if (this.cfgBlocksState.containsKey(pred)) {
+				out.and(this.cfgBlocksState.get(pred).getOut());
 			}
 		}
 		
 		calculateGenKillSets(block, bFlow);
 		
 		// Calculate Out
-		BitSet in = bFlow.getIn(); // IN = (OUT - KILL) U GEN
+		BitSet in = bFlow.getOut(); // IN = (OUT - KILL) U GEN
 		in.or(out);
 		in.xor(bFlow.getKill());
 		in.or(bFlow.getGen());
 		
 		if (!in.equals(origIn)) {
-			// Add predecessors to cfgBlocks list
-			for (CFGBlock pred : block.getPredecessors()) {
-				if (!cfgBlocksToProcess.contains(pred)) {
-					cfgBlocksToProcess.add(pred);
+			// Add successors to cfgBlocks list
+			for (CFGBlock succ : block.getSuccessors()) {
+				if (!cfgBlocksToProcess.contains(succ)) {
+					cfgBlocksToProcess.add(succ);
 				}
 			}
 		}
@@ -144,10 +148,9 @@ public class ReachingStoresAnalyzer {
 		return bFlow;
 	}
 	
-	private void calculateGenKillSets(CFGBlock block, BlockDataFlowState bFlow) {		
+	private void calculateGenKillSets(CFGBlock block, BlockDataFlowState bFlow) {
 		for (int i = block.getStatements().size()-1; i >= 0; i--) {
 			LIRStatement stmt = block.getStatements().get(i);
-			
 			if (stmt.getClass().equals(QuadrupletStmt.class)) {
 				QuadrupletStmt qStmt = (QuadrupletStmt)stmt;
 				if (!qStmt.getDestination().getClass().equals(RegisterName.class)) {
@@ -155,8 +158,7 @@ public class ReachingStoresAnalyzer {
 				}
 			}
 			else if (stmt.getClass().equals(StoreStmt.class)) {
-				StoreStmt sStmt = (StoreStmt) stmt;
-				updateKillGenSet(block.getMethodName(), sStmt, bFlow);
+				updateKillGenSet(block.getMethodName(), (StoreStmt)stmt, bFlow);
 			}
 			else if (stmt.getClass().equals(CallStmt.class)) {
 				updateKillGenSet(block.getMethodName(), (CallStmt)stmt, bFlow);
@@ -164,18 +166,10 @@ public class ReachingStoresAnalyzer {
 		}
 	}
 	
-	private void updateKillGenSet(String methodName, StoreStmt stmt,	BlockDataFlowState bFlow) {
-		if (stmt == null) return;
-		
-		BitSet gen = bFlow.getGen();
-		
-		gen.set(this.uniqueStores.get(methodName).indexOf(stmt.getVariable()));	
-	}
-
 	private void updateKillGenSet(String methodName, CallStmt stmt, BlockDataFlowState bFlow) {
 		if (stmt.getMethodLabel().equals(ProgramFlattener.exceptionHandlerLabel)) return;
 		
-		// Kill all globals!
+		// Kill all stores!
 		for (int i = 0; i < bFlow.getOut().size(); i++) {
 			if (bFlow.getOut().get(i)) {
 				bFlow.getKill().set(i);
@@ -185,17 +179,23 @@ public class ReachingStoresAnalyzer {
 		bFlow.getGen().clear();
 	}
 
+	private void updateKillGenSet(String methodName, StoreStmt stmt, BlockDataFlowState bFlow) {
+		if (stmt == null) return;
+		
+		BitSet gen = bFlow.getGen();
+		
+		gen.set(this.uniqueGlobals.get(methodName).indexOf(stmt.getVariable()));	
+	}
+
 	public void updateKillGenSet(String methodName, QuadrupletStmt stmt, BlockDataFlowState bFlow) {
 		if (stmt == null) return;
 		
-		BitSet out = bFlow.getOut();
+		BitSet in = bFlow.getIn();
 		BitSet gen = bFlow.getGen();
 		BitSet kill = bFlow.getKill();
 		
-		for (Name name : this.uniqueStores.get(methodName)) {
-			if (name.equals(stmt.getDestination())) continue; // One store can cover multiple assignments
-			
-			int i = this.uniqueStores.get(methodName).indexOf(name);
+		for (Name name : this.uniqueGlobals.get(methodName)) {
+			int i = this.uniqueGlobals.get(methodName).indexOf(name);
 			
 			boolean resetName = false;
 			
@@ -224,11 +224,15 @@ public class ReachingStoresAnalyzer {
 			}
 			
 			if (resetName) {
-				if (out.get(i)) {
+				if (in.get(i)) {
 					kill.set(i);
 				}
 				
 				gen.clear(i);
+			}
+			
+			if (name.equals(stmt.getDestination())) {
+				gen.set(i);
 			}
 		}		
 	}
@@ -242,7 +246,11 @@ public class ReachingStoresAnalyzer {
 		this.cfgBlocksState = cfgBlocksState;
 	}
 
-	public HashMap<String, List<Name>> getStores() {
-		return uniqueStores;
+	public HashMap<String, List<Name>> getUniqueGlobals() {
+		return uniqueGlobals;
+	}
+
+	public void setUniqueGlobals(HashMap<String, List<Name>> uniqueGlobals) {
+		this.uniqueGlobals = uniqueGlobals;
 	}
 }
--- Compiler/src/decaf/memory/LoadStmtsAdder.java
+++ Compiler/src/decaf/memory/LoadStmtsAdder.java
@@ -4,11 +4,11 @@ import java.util.HashMap;
 
 import decaf.dataflow.cfg.MethodIR;
 
-public class ExplicitLoadAdder {
+public class LoadStmtsAdder {
 	private NaiveLoadAdder la;
 	private NaiveLoadOptimizer lo;
 	
-	public ExplicitLoadAdder(HashMap<String, MethodIR> mMap) {
+	public LoadStmtsAdder(HashMap<String, MethodIR> mMap) {
 		this.la = new NaiveLoadAdder(mMap);
 		this.lo = new NaiveLoadOptimizer(mMap);
 	}
